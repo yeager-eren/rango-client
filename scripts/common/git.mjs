699a35a2dfa9bc30a18287b95269ee2c0eb7a3ea
Eren Yeager <eren@rango.exchange>
2023-12-19 10:33:53 +0000

    fix

--- scripts/common/git.mjs
+++ scripts/common/git.mjs
@@ -166,7 +166,7 @@ export async function publishCommitAndTags(pkgs, { skipGitTagging }) {
   });
 
   // Creating annotated tags based on packages
-  if (!isTaggingSkipped) {
+  if (isTaggingSkipped) {
     await publishTags(pkgs);
   }
 
