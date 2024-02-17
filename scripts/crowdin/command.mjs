acf72d5614eef20dbb6c07d3adacefcc0459f2c8
Nikaru <nikaru@rango.exchange>
2024-02-17 15:48:02 +0000

    fix: fix github comments

--- scripts/crowdin/command.mjs
+++ scripts/crowdin/command.mjs
@@ -1,14 +1,15 @@
-const projectId = process.env.CROWDIN_PROJECT_ID;
-const token =  process.env.CROWDIN_PERSONAL_TOKEN;
+import { CrowdinError } from "../common/errors.mjs";
 
-const BASE_URL = `https://api.crowdin.com/api/v2/projects/${projectId}`;
-const preTranslateURL = `${BASE_URL}/pre-translations`;
+const PROJECT_ID = process.env.CROWDIN_PROJECT_ID;
+const TOKEN =  process.env.CROWDIN_PERSONAL_TOKEN;
+
+const BASE_URL = `https://api.crowdin.com/api/v2`;
+const BASE_PROJECT_URL = `${BASE_URL}/projects/${PROJECT_ID}`;
 const REQUEST_INTERVAL_TIMEOUT = 10_000;
-const MAXIMUM_REQUEST_RETRIES = 12;
+const MAXIMUM_PRETRANSLATION_STATUS_CHECK = 20;
 
 const requestData = {
   method: 'mt',
-  engineId: 410374,
   autoApproveOption: 'all',
   duplicateTranslations: false,
   skipApprovedTranslations: true,
@@ -18,14 +19,14 @@ const requestData = {
 
 const getMachineTranslationEngineID = async () =>{
   try {
-    const response = await fetch('https://api.crowdin.com/api/v2/mts', {
+    const response = await fetch(`${BASE_URL}/mts`, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${TOKEN}`,
       },
     });
 
     if (!response.ok) {
-      throw new Error(`Failed to get target language ids`);
+      throw 'Failed to get machine translation data';
     }
 
     const responseData = await response.json();
@@ -34,21 +35,21 @@ const getMachineTranslationEngineID = async () =>{
     return engineId;
 
   } catch (error) {
-    console.error('Error:', error);
-    throw error;
+    console.error('Error:', error);    
+    throw new CrowdinError(error);
   }
 }
 
 const getLanguageIds = async () => {
   try {
-    const response = await fetch(`${BASE_URL}`, {
+    const response = await fetch(BASE_PROJECT_URL, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${TOKEN}`,
       },
     });
 
     if (!response.ok) {
-      throw new Error(`Failed to get target language ids`);
+      throw 'Failed to get target language ids';
     }
 
     const responseData = await response.json();
@@ -57,20 +58,20 @@ const getLanguageIds = async () => {
     return languageIds;
   } catch (error) {
     console.error('Error:', error);
-    throw error;
+    throw new CrowdinError(error);
   }
 };
 
 const getSourceFileId = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/files`, {
+    const response = await fetch(`${BASE_PROJECT_URL}/files`, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${TOKEN}`,
       },
     });
 
     if (!response.ok) {
-      throw new Error(`Failed to get source file id`);
+      throw 'Failed to get source file id';
     }
 
     const responseData = await response.json();
@@ -79,23 +80,26 @@ const getSourceFileId = async () => {
     return sourceFileId;
   } catch (error) {
     console.error('Error:', error);
-    throw error;
+    throw new CrowdinError(error);
   }
 };
 
 const sendPreTranslateRequest = async ({ sourceFileId, languageIds }) => {
   try {
-    const response = await fetch(preTranslateURL, {
+    const engineId = await getMachineTranslationEngineID();
+    console.log('engineId', engineId);
+    requestData.engineId = engineId ;
+    const response = await fetch(`${BASE_PROJECT_URL}/pre-translations`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${TOKEN}`,
       },
       body: JSON.stringify({ ...requestData, fileIds: [sourceFileId], languageIds }),
     });
 
     if (!response.ok) {
-      throw new Error(`Failed to initiate pre-translation. Status: ${response.status}`);
+      throw `Failed to initiate pre-translation. Status: ${response.status}`;
     }
 
     const responseData = await response.json();
@@ -104,26 +108,26 @@ const sendPreTranslateRequest = async ({ sourceFileId, languageIds }) => {
     return preTranslationId;
   } catch (error) {
     console.error('Error:', error);
-    throw error;
+    throw new CrowdinError(error);
   }
 };
 
 const checkPreTranslateStatus = async (preTranslationId) => {
-  const maxAttempts = MAXIMUM_REQUEST_RETRIES;
+  const maxAttempts = MAXIMUM_PRETRANSLATION_STATUS_CHECK;
   let attempt = 0;
 
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
   while (attempt < maxAttempts) {
     try {
-      const response = await fetch(`${preTranslateURL}/${preTranslationId}`, {
+      const response = await fetch(`${BASE_PROJECT_URL}/pre-translations/${preTranslationId}`, {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${TOKEN}`,
         },
       });
 
       if (!response.ok) {
-        throw new Error(`Failed to get pre-translation status. Status: ${response.status}`);
+        throw `Failed to get pre-translation status. Status: ${response.status}`;
       }
 
       const responseData = await response.json();
@@ -140,16 +144,20 @@ const checkPreTranslateStatus = async (preTranslationId) => {
       }
     } catch (error) {
       console.error('Error:', error);
-      throw error;
+      throw new CrowdinError(error);
     }
   }
-
-  throw new Error('Timeout: Pre-translation did not succeed within the specified time.');
+  throw new CrowdinError('Timeout: Pre-translation did not succeed within the specified time.');
 };
 
 (async () => {
   try {
+    if(!PROJECT_ID || !PROJECT_ID){
+      throw new CrowdinError('environments are not set correctly');
+    }
     const [sourceFileId, languageIds] = await Promise.all([getSourceFileId(), getLanguageIds()]);
+    console.log('sourceFileId', sourceFileId);
+    console.log('languageIds', languageIds);
     const preTranslationId = await sendPreTranslateRequest({ sourceFileId, languageIds });
     await checkPreTranslateStatus(preTranslationId);
   } catch (error) {
